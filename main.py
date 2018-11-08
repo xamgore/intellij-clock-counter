c6432ef30085ce83bd25a6c9062d52a5d30e3dc5
xamgore <xamgore@ya.ru>
2018-11-08 04:20:02 +0300

    Notify only when plugin's data has changed

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ from telegram.utils.request import Request
 
 from plugin import Plugin
 
-chat_id = 108562525
-plugins_ids = [7754, 10050, 11252]
-token = Path(sys.path[0] + '/token.txt').read_text().strip()
+my_plugin_id = 11252
+plugins_ids = [7754, 10050, my_plugin_id]
+plugins = {id: Plugin.load(id) for id in plugins_ids}
 
-if __name__ == '__main__':
-    msg = Plugin.stringify(list(map(Plugin.load, plugins_ids)))
-    print(msg, end='\n\n')
+telegram_chat_id = 108562525
+telegram_bot_token = Path(sys.path[0] + '/token.txt').read_text().strip()
 
+
+def send_message(chat_id, msg, token):
     request = Request(
         proxy_url='socks5://phobos.public.opennetwork.cc:1090',
         urllib3_proxy_kwargs={'username': '108562525', 'password': 'oiqjv2ZD'})
@@ -23,5 +24,22 @@ if __name__ == '__main__':
     basicConfig(level=DEBUG, format='%(asctime)s - %(name)s - %(levelname)s\n%(message)s\n')
 
     Bot(token, request=request) \
-        .sendMessage(chat_id, '`{}`'.format(msg),
-                     parse_mode=ParseMode.MARKDOWN, disable_notification=True)
+        .sendMessage(chat_id, msg, parse_mode=ParseMode.MARKDOWN, disable_notification=True)
+
+
+def there_are_changes():
+    store = Path(sys.path[0] + '/store.txt')
+    content = store.read_text() if store.exists() else ''
+    my_plugin = repr(plugins[my_plugin_id])
+
+    if my_plugin.strip() != content.strip():
+        store.write_text(my_plugin)
+        return True
+
+
+if __name__ == '__main__':
+    if there_are_changes():
+        msg = Plugin.stringify(plugins.values())
+        print(msg, end='\n\n')
+
+        send_message(telegram_chat_id, '`{}`'.format(msg), telegram_bot_token)
