e65675ec3d1b131d89fce8daa1dd7a226c92c322
xamgore <xamgore@ya.ru>
2018-11-08 03:53:45 +0300

    Log telegram bot actions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S pipenv run python
 import sys
 
+from logging import DEBUG, basicConfig
 from pathlib import Path
 from telegram import Bot, ParseMode
 from telegram.utils.request import Request
@@ -13,11 +14,13 @@ token = Path(sys.path[0] + '/token.txt').read_text().strip()
 
 if __name__ == '__main__':
     msg = Plugin.stringify(list(map(Plugin.load, plugins_ids)))
-    print(msg)
+    print(msg, end='\n\n')
 
     request = Request(
         proxy_url='socks5://phobos.public.opennetwork.cc:1090',
         urllib3_proxy_kwargs={'username': '108562525', 'password': 'oiqjv2ZD'})
+
+    basicConfig(level=DEBUG, format='%(asctime)s - %(name)s - %(levelname)s\n%(message)s\n')
 
     Bot(token, request=request) \
         .sendMessage(chat_id, '`{}`'.format(msg),
